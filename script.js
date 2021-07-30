9031e8e01eb331279a6ca572003b849fb4cf3f42
unknown <max_erling@outlook.com>
2021-07-30 21:52:54 +0200

    created function adds selected class based on todays date

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log('looog');
+var todaysDateHighlight = function () {
+    var today = new Date();
+    var splittedString = today.toString().split(' ');
+    var eleNum;
+    if (checkMonth(splittedString[1]) && ((eleNum = checkDay(splittedString[0] + ' ' + splittedString[2])) != -1)) {
+        var todayEle = document.getElementById("day-" + eleNum);
+        todayEle.classList.add('selected');
+    }
+};
+todaysDateHighlight();
+function checkMonth(currentMonth) {
+    var monthEle = document.getElementById('cal-month');
+    if (monthEle.textContent.includes(currentMonth)) {
+        return true;
+    }
+    return false;
+}
+;
+function checkDay(currentDay) {
+    var currentWeek = document.querySelector('.cal-head').childNodes[1].textContent.split("\n");
+    for (var i = 0; i < currentWeek.length; i++) {
+        if (currentWeek[i].includes("" + currentDay.toUpperCase())) {
+            return (i - 1);
+        }
+    }
+    return -1;
+}
+;
+module.exports = {};
--- script.ts
+++ script.ts
@@ -1,1 +1,48 @@
-console.log('looog')
+const todaysDateHighlight = function () {
+  const today : Date  = new Date();
+  const splittedString : string[] = today.toString().split(' ');
+  let eleNum : number;
+  if (checkMonth(splittedString[1]) && ( (eleNum = checkDay(splittedString[0] + ' ' + splittedString[2])) != -1)) {
+    const todayEle = document.getElementById(`day-${eleNum}`);
+    todayEle.classList.add('selected')
+  }
+
+ 
+};
+
+todaysDateHighlight();
+
+
+
+
+
+
+
+
+
+
+
+function checkMonth(currentMonth : string) : boolean {
+  const monthEle = document.getElementById('cal-month');
+  if (monthEle.textContent.includes(currentMonth)) {
+    return true;
+  }
+
+  return false;
+
+};
+function checkDay(currentDay : string) : number {
+  const currentWeek = document.querySelector('.cal-head').childNodes[1].textContent.split(`\n`);
+  for (let i : number = 0; i < currentWeek.length; i++) {
+    if (currentWeek[i].includes(`${currentDay.toUpperCase()}`)) {
+      return (i-1);
+    }
+  }
+  
+  
+  return -1;
+};
+
+
+
+export = {};
