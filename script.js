09ab065f9591d3703c0a33011a3fed64249b9457
unknown <max_erling@outlook.com>
2021-07-31 21:55:38 +0200

    if not selected, move forward monday until find selected

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 "use strict";
-todaysDateHighlight();
-function todaysDateHighlight() {
-    var today = new Date();
-    var splittedString = today.toString().split(' ');
-    var eleNum;
-    if (checkMonth(splittedString[1]) && ((eleNum = checkDay(splittedString[0] + ' ' + splittedString[2])) != -1)) {
-        var todayEle = document.getElementById("day-" + eleNum);
-        todayEle.classList.add('selected');
+checkCurrentWeek();
+function checkCurrentWeek() {
+    for (let i = 0; i < 3; i++) {
+        if (!todaysDateHighlight()) {
+            moveForwardWeek();
+        }
     }
+}
+function moveForwardWeek() {
+    const weekdayParentEle = document.getElementById('weekdays-name');
+    const monthAndYearEle = document.getElementById('cal-month');
+    const p = document.createElement("p");
+    p.textContent = "Hello, World!";
+    const monthAndYearArray = monthAndYearEle?.textContent?.split(' ') ?? [];
+    const weekday = Number(weekdayParentEle?.children[1]?.textContent?.split(' ')[1].substring(0, 2));
+    const randDate = new Date(`${monthAndYearArray[0]} ${weekday}, ${monthAndYearArray[1]}`);
+    const randDateOneWeekForward = new Date(randDate.getFullYear(), randDate.getMonth(), randDate.getDate() + 7);
+    console.log(randDateOneWeekForward);
+    const dateOWFArray = randDateOneWeekForward.toString().split(' ');
+    dateOWFArray[1] = randDateOneWeekForward.toLocaleString('default', { month: 'long' });
+    weekdayParentEle.children[1].textContent = `${dateOWFArray[0].toUpperCase()} ${dateOWFArray[2]}/${randDateOneWeekForward.getMonth() + 1}`;
+    monthAndYearEle.textContent = `${dateOWFArray[1]} ${dateOWFArray[3]}` ?? '';
+    // change current mon to randDateOneWeekForward, test it with wrong dates, implement for all days with loop, 
+    //extra: change so the default date is based on current day (ex: day-1 is the closest mon)
+}
+function todaysDateHighlight() {
+    const today = new Date();
+    const splittedString = today.toString().split(' ');
+    let eleNum;
+    if (checkMonth(splittedString[1]) && ((eleNum = checkDay(splittedString[0] + ' ' + splittedString[2])) != -1)) {
+        const todayEle = document.getElementById(`day-${eleNum}`);
+        todayEle?.classList.add('selected');
+        return true;
+    }
+    return false;
 }
 ;
 function checkMonth(currentMonth) {
-    var monthEle = document.getElementById('cal-month');
-    if (monthEle.textContent.includes(currentMonth)) {
+    const monthEle = document.getElementById('cal-month');
+    if (monthEle?.textContent.includes(currentMonth)) {
         return true;
     }
     return false;
 }
 ;
 function checkDay(currentDay) {
-    var currentWeek = document.querySelector('.cal-head').childNodes[1].textContent.split("\n");
-    for (var i = 0; i < currentWeek.length; i++) {
-        if (currentWeek[i].includes("" + currentDay.toUpperCase())) {
+    const currentWeek = document?.querySelector('.cal-head')?.childNodes[1]?.textContent?.split(`\n`) ?? [];
+    for (let i = 0; i < currentWeek.length; i++) {
+        if (currentWeek[i].includes(`${currentDay.toUpperCase()}`)) {
             return (i - 1);
         }
     }
--- script.ts
+++ script.ts
@@ -1,25 +1,61 @@
-todaysDateHighlight();
+
+checkCurrentWeek();
 
 
 
 
 
 
+function checkCurrentWeek() : void {
+  for (let i = 0; i < 3 ; i++) {
+    if (!todaysDateHighlight()) {
+      moveForwardWeek()
+    } 
+  }
+
+
+  
+
+  
+}
+
+
+function moveForwardWeek() : void {
+  const weekdayParentEle  = document.getElementById('weekdays-name');
+  const monthAndYearEle = document.getElementById('cal-month');
+
+  const p = document.createElement("p");
+  p.textContent = "Hello, World!";
+  const monthAndYearArray : string[] = monthAndYearEle?.textContent?.split(' ') ?? [];
+  const weekday : number = Number(weekdayParentEle?.children[1]?.textContent?.split(' ')[1].substring(0,2));
+
+  const randDate : Date = new Date(`${monthAndYearArray[0]} ${weekday}, ${monthAndYearArray[1]}`);
+  const randDateOneWeekForward : Date = new Date(randDate.getFullYear(),randDate.getMonth(),randDate.getDate()+7);
+  console.log(randDateOneWeekForward)
+  const dateOWFArray : string[] = randDateOneWeekForward.toString().split(' '); 
+  dateOWFArray[1] = randDateOneWeekForward.toLocaleString('default', {month: 'long'});
+  weekdayParentEle!.children[1].textContent = `${dateOWFArray[0] .toUpperCase()} ${dateOWFArray[2]}/${randDateOneWeekForward.getMonth()+1}`;
+  monthAndYearEle!.textContent! = `${dateOWFArray[1]} ${dateOWFArray[3]}` ?? '';
+  
+  // change current mon to randDateOneWeekForward, test it with wrong dates, implement for all days with loop, 
+  
+  //extra: change so the default date is based on current day (ex: day-1 is the closest mon)
+}
 
 
 
 
-
-
-function todaysDateHighlight() : void {
+function todaysDateHighlight() : boolean {
   const today : Date  = new Date();
   const splittedString : string[] = today.toString().split(' ');
   let eleNum : number;
   if (checkMonth(splittedString[1]) && ( (eleNum = checkDay(splittedString[0] + ' ' + splittedString[2])) != -1)) {
     const todayEle = document.getElementById(`day-${eleNum}`);
-    todayEle.classList.add('selected')
+    todayEle?.classList.add('selected')
+    return true;
   }
 
+  return false;
  
 };
 
@@ -27,7 +63,7 @@ function todaysDateHighlight() : void {
 
 function checkMonth(currentMonth : string) : boolean {
   const monthEle = document.getElementById('cal-month');
-  if (monthEle.textContent.includes(currentMonth)) {
+  if (monthEle?.textContent!.includes(currentMonth)) {
     return true;
   }
 
@@ -35,7 +71,7 @@ function checkMonth(currentMonth : string) : boolean {
 
 };
 function checkDay(currentDay : string) : number {
-  const currentWeek = document.querySelector('.cal-head').childNodes[1].textContent.split(`\n`);
+  const currentWeek : string[] = document?.querySelector('.cal-head')?.childNodes[1]?.textContent?.split(`\n`) ?? [];
   for (let i : number = 0; i < currentWeek.length; i++) {
     if (currentWeek[i].includes(`${currentDay.toUpperCase()}`)) {
       return (i-1);
